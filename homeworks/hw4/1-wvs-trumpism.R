73409df5a8f028b6ff4e4cb3e617eb5acbf546c2
svmiller <steven.v.miller@gmail.com>
2020-11-16 10:31:33 -0500

    oops. Sorry, Kristen :/

--- homeworks/hw4/1-wvs-trumpism.R
+++ homeworks/hw4/1-wvs-trumpism.R
@@ -6,20 +6,20 @@ WVS <- readRDS(url("http://posc3410.svmiller.com/toy-data/wvs-trumpism.rds"))
 M1 <- glm(sldummy ~ z_age + I(z_age^2) + female +
             hsedorless + z_ideo + z_incscale + gop + unemployed +
             + z_lemanc + gop*hsedorless,
-          data=subset(Data), family=binomial(link = "logit"))
+          data=subset(WVS), family=binomial(link = "logit"))
 
 summary(M1)
 
 M2 <- glm(sldummy ~ z_age + I(z_age^2) + female +
             hsedorless + z_ideo + z_incscale + gop + unemployed +
             + z_lemanc + gop*hsedorless,
-          data=subset(Data, year == 2011), family=binomial(link = "logit"))
+          data=subset(WVS, year == 2011), family=binomial(link = "logit"))
 
 summary(M2)
 
 M3 <- glm(hddummy ~ z_age + I(z_age^2) + female +
             hsedorless + z_ideo + z_incscale + gop + unemployed +
             + z_lemanc + gop*hsedorless,
-          data=subset(Data), family=binomial(link = "logit"))
+          data=subset(WVS), family=binomial(link = "logit"))
 
 summary(M3)
