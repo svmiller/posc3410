13200984be00a8789278e802c793d7c2178f5edd
svmiller <steven.v.miller@gmail.com>
2022-01-21 10:45:11 -0500

    add

--- homeworks/hw5/1-wvs-usa-abortion.R
+++ homeworks/hw5/1-wvs-usa-abortion.R
@@ -1,99 +1,11 @@
 library(tidyverse)
-# library(stevemisc) # Functionally optional
+library(stevemisc) 
 
 
 Abortion <- readRDS(url("http://posc3410.svmiller.com/toy-data/wvs-usa-abortion.rds"))
 
-# Some helper functions
-# NOTE: these are available in stevemisc by default
-# If you load stevemisc, you won't need to load these functions since this will just overwrite them
-# There's no harm in doing it, but that's just what it'll be doing
-
-r2sd <- function(x, na.rm=T) {return ((x-mean(x,na.rm=na.rm))/(2*sd(x, na.rm=na.rm)))}
-
-get_sims <- function(model, newdata, nsim, seed){
-  # require(arm)
-  if(missing(seed)) {
-    
-  } else {
-    set.seed(seed)
-  }
-  modelsim <- arm::sim(model, n.sims=nsim)
-  MM = model.matrix(terms(model),newdata)
-  Sims <- tibble(y = numeric(),
-                 sim = numeric())
-  for(i in (1:nsim)) {
-    output <- NULL
-    if (is(model,"lm") == TRUE | is(model,"glm") == TRUE) {
-      yi <- MM %*% coef(modelsim)[i,] }
-    else { # assuming it's merMod
-      yi <- MM %*% coef(modelsim)$fixef[i,]
-    }
-    sim<-rep(i, length (yi))
-    hold_me <- suppressMessages(as_tibble(cbind(yi, sim))) %>% rename(y = V1)
-    Sims <- bind_rows(Sims, hold_me)
-  }
-  return(Sims)
-  
-}
-
-show_ranef <- function(data, grp, reorder = TRUE){
-  require(broom)
-  # https://stackoverflow.com/questions/34344599/a-caterpillar-plot-of-just-the-significant-random-effects-from-a-mixed-effects
-  augment.ranef.mer <- function(x,
-                                ci.level=0.9,
-                                reorder=TRUE,
-                                order.var=1) {
-    tmpf <- function(z) {
-      if (is.character(order.var) && !order.var %in% names(z)) {
-        order.var <- 1
-        warning("order.var not found, resetting to 1")
-      }
-      ## would use plyr::name_rows, but want levels first
-      zz <- data.frame(level=rownames(z),z,check.names=FALSE)
-      if (reorder) {
-        ## if numeric order var, add 1 to account for level column
-        ov <- if (is.numeric(order.var)) order.var+1 else order.var
-        zz$level <- reorder(zz$level, zz[,order.var+1], FUN=identity)
-      }
-      ## Q-Q values, for each column separately
-      qq <- c(apply(z,2,function(y) {
-        qnorm(ppoints(nrow(z)))[order(order(y))]
-      }))
-      rownames(zz) <- NULL
-      pv   <- attr(z, "postVar")
-      cols <- 1:(dim(pv)[1])
-      se   <- unlist(lapply(cols, function(i) sqrt(pv[i, i, ])))
-      ## n.b.: depends on explicit column-major ordering of se/melt
-      zzz <- cbind(reshape2::melt(zz,id.vars="level",value.name="estimate"),
-                   qq=qq,std.error=se)
-      ## reorder columns:
-      subset(zzz,select=c(variable, level, estimate, qq, std.error))
-    }
-    dd <- plyr::ldply(x,tmpf,.id="grp")
-    ci.val <- -qnorm((1-ci.level)/2)
-    transform(dd,
-              p=2*pnorm(-abs(estimate/std.error)), ## 2-tailed p-val
-              lb=estimate-ci.val*std.error,
-              ub=estimate+ci.val*std.error)
-  }
-  require(ggplot2)
-  data <- augment(ranef(data,condVar=TRUE))
-  if(reorder) {
-    data <- data[data$grp == grp,]
-    data$level <- as.character(data$level)
-  }
-  else {}
-  ggplot(data[data$grp == grp,],aes(estimate, level,xmin=lb,xmax=ub))+
-    geom_errorbarh(height=0)+
-    geom_vline(xintercept=0,lty=2)+
-    geom_point()+facet_wrap(~variable,scale="free_x") +
-    ylab("Levels of the Random Effect") +
-    xlab("Estimated Intercept")
-}
-
 # Install necessary packages first (if you don't have them).
-# I tried to keep this as miminal as possible, but you'll need some additional packages.
+# I tried to keep this as minimal as possible, but you'll need some additional packages.
 # That's my bad. Sorry.
 
 list.of.packages <- c("broom","arm","lme4","modelr")
